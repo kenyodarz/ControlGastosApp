a68acdd45e465f5167de000e18fec740bbac3349
Jorge Mina <minajorge@hotmail.com>
2020-07-16 11:55:36 -0500

    Update Reporte

--- src/app/components/reporte/reporte.component.ts
+++ src/app/components/reporte/reporte.component.ts
@@ -91,6 +91,36 @@ export class ReporteComponent implements OnInit {
       })
       .save();
   }
+  exportExcel() {
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.table_to_sheet(
+        document.getElementById('id')
+      );
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(
+        excelBuffer,
+        'GVSC-' + this.selectedInforme.idInforme
+      );
+    });
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import('file-saver').then((FileSaver) => {
+      let EXCEL_TYPE =
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+      let EXCEL_EXTENSION = '.xlsx';
+      const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE,
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+      );
+    });
+  }
 
   obtenerDesembolsos() {
     this.rEntrada.getAll().subscribe((result: any) => {
